5da775730a74b3da101693b64fad94435570d255
Harsh Kedia <harshkedi@gmail.com>
2020-05-15 00:15:27 -0700

    added model viewer and query params

--- web-viewer/src/App.vue
+++ web-viewer/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <div id="nav" />
     <transition name="fade">
       <router-view />
     </transition>
@@ -12,11 +9,13 @@
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  box-sizing: border-box;
+  width: 1200px;
+  margin: 0 auto;
 }
 
 #nav {
--- web-viewer/src/views/Home.vue
+++ web-viewer/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <model-viewer
+      class="model"
+      :src="url"
+      ar
+      ar-modes="webxr scene-viewer quick-look fallback"
+      ar-scale="auto"
+      magic-leap
+      camera-controls
+      alt="A 3D model of an astronaut"
+      :ios-src="urlIos"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "../components/HelloWorld.vue";
+import "@google/model-viewer";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  computed: {
+    url() {
+      return `http://ec2-13-233-130-134.ap-south-1.compute.amazonaws.com/models/${this.$route.query.name}.glb`;
+    },
+    urlIos() {
+      return `http://ec2-13-233-130-134.ap-south-1.compute.amazonaws.com/models/${this.$route.query.name}.usdz`;
+    }
   }
 };
 </script>
+
+<style scoped>
+.model {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  box-sizing: border-box;
+  height: 800px;
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
